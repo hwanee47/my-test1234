92b9d0b041720fe469208794a5f923dd8f6e8e9d
hwaneehwanee <hwaneehwanee@gmail.com>
2025-11-19 23:14:09 +0900

    .

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,4 +6,5 @@ export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Checkbox } from './Checkbox';
 export { default as Datepicker } from './Datepicker';
+export { default as Input } from './Input';
 export { default as Spinner } from './Spinner';
